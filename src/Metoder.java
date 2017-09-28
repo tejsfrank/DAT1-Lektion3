a428fc74329eaae6838b7d8e3842c0d7fc87f269
Andras Acs <acsandras@gmail.com>
2017-09-28 13:19:10 +0200

    Ændringer i klassen

--- src/Metoder.java
+++ src/Metoder.java
@@ -1,29 +1,58 @@
 public class Metoder {
 
+    public int x;
+
     public static void main(String[] args) {
+        System.out.println(1+2);
 
-        helloWorld(); // Statiske metoder kan kaldes uden instantiering af klassen
+        Metoder m = new Metoder();
+        int resultat = m.sum(10, 20);
+        System.out.println(resultat);
+        resultat = m.sum(15, 25);
+        System.out.println(resultat);
 
-        Metoder m = new Metoder(); // Class methods kan kun køres på en instans af klassen
+        Metoder.udskrivHej();
+        udskrivHej();
+        udskrivHej();
 
-        m.hello("András");
+        double skabnen = (int) 6*Math.random()+1;
+        System.out.println(skabnen);
 
-        int resultat;
-        resultat = m.add(1, 2);
-        System.out.println("Resultatet er " + resultat);
+        System.out.println(m.sum3(8, 5 , 10));
+
+        indtastOgVisNavn("András");
 
     }
 
-    private int add(int a, int b) {
-        return a + b;
+    public Metoder() {
+
     }
 
-    public void hello(String name) {
-        System.out.println("Hello " + name + "!");
+    public int sum(int a, int b) {
+        // Her implementer jeg metoder
+        int resultat = a + b;
+        return resultat;
+        /*
+        1
+        2
+        3
+        4
+        5
+        6
+        7
+         */
     }
 
-    public static void helloWorld() {
-        System.out.println("Hello World!");
+    public static void udskrivHej() {
+        System.out.println("Hej");
+    }
+
+    public int sum3(int a, int b,int c) {
+        return a + b +c;
+    }
+
+    public static void indtastOgVisNavn(String navn) {
+        System.out.println("Hej " + navn);
     }
 
 }
